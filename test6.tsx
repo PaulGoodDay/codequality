bb17457757440439b796bf2b00be71ed49fecc7d
Paul Buendia <pbuendia@echobind.com>
2022-09-09 14:55:53 -0700

    file dup 10x

---
+++ test6.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const x = true ? 1 : false ? 0: 1;
+  return (
+    <View style={styles.container}>
+      <Text>I changed this a third time!</Text>
+    </View>
+  );
+}
+
+export const test = () => {
+  const x = true ? 1 : false ? 0: 1;
+  return (
+    <View style={styles.container}>
+      <Text>I changed this a third time!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
