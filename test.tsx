bcee7b77ec624c29798981572372357ddc32a3fb
Paul Buendia <pbuendia@echobind.com>
2022-09-09 14:10:42 -0700

    push dups

--- test.tsx
+++ test.tsx
@@ -1,4 +1,12 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+      <View style={styles.container}>
+        <Text>I changed this a third time!</Text>
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
