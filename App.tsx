762c28d1f33f211f74fe267ca53495372d89732c
Paul Buendia <pbuendia@echobind.com>
2022-09-09 14:25:06 -0700

    nested ternary

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const x = true ? 1 : false ? 0: 1;
   return (
     <View style={styles.container}>
       <Text>I changed this a third time!</Text>
